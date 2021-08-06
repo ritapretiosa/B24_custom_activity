1be11635084fe12c2b576600095e1908b4f802dc
ritapretiosa <59686956+ritapretiosa@users.noreply.github.com>
2021-08-06 16:47:24 +0500

    changetaskparticipants

---
+++ changetaskparticipants/lang/ru/.description.php
@@ -0,0 +1,7 @@
+<?php
+defined('B_PROLOG_INCLUDED') || die;
+
+$MESS['CTP_NAME'] = "Смена участников задачи";
+$MESS['CTP_DESCRIPTION'] = 'Изменение пользователей, учавствующих в задаче';
+$MESS['CTP_RETURN_RESULT'] = "Успешное выполнение (да/нет)";
+$MESS['CTP_RETURN_ERROR_TEXT'] = "Текст ошибки";
